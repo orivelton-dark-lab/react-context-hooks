b8d1f92d2cbb6bcc7a4fc323b038dad47ef9828f
Örivelton Cesar <oconceicao@recordtv.com.br>
2019-06-15 05:18:29 -0300

    chore: Component Booklist and Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav>
+        <h1>
+          Book App
+        </h1>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
